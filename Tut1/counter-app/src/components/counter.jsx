51e0aad4fe56290ef2cbfc9738b657f31ec3317e
bishojbk <bishojbk@gmail.com>
2022-11-29 15:03:41 +0545

    1 hour

---
+++ Tut1/counter-app/src/components/counter.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+  };
+
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClass()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          <li>Tag1</li>
+          <li>Tag2</li>
+        </ul>
+      </div>
+    );
+  }
+  getBadgeClass() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count == 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
